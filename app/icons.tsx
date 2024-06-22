47540cb84cedacaf5b4892ef408543cef67d0cc4
Rizky <rizky@avolut.com>
2024-06-22 00:33:43 -0700

    fix

---
+++ app/icons.tsx
@@ -0,0 +1,27 @@
+import {
+  PieChart,
+  Box,
+  Ticket,
+  PencilRuler,
+  Drill,
+  NotebookPen,
+  UserSearch,
+  User,
+  Bell,
+  LogOut,
+  Settings,
+} from "lucide-react";
+
+export const icons = {
+  PieChart,
+  Box,
+  Ticket,
+  PencilRuler,
+  Drill,
+  NotebookPen,
+  UserSearch,
+  User,
+  Bell,
+  LogOut,
+  Settings,
+};
---
+++ index.tsx
@@ -0,0 +1,4 @@
+import "@/utils/init";
+import "app/css/build.css";
+export * from "@/exports";
+export * from "app/icons";
